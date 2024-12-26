a45d717e0de33e582ee4ca284deb6a8cbe02b775
Kori Nikhil <154355081+korinikhil@users.noreply.github.com>
2024-12-26 18:12:01 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Navigate to="/home" />} />
-            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
             <Route path='/about' element={<About />} />
             <Route path='/contact' element={<Contact />} />
             <Route path='/catalogue' element={<Catalogue />} />
